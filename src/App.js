7ab0b52274620ff1b1ab872aedd18792edc833cf
z4cope <63551733+z4cope@users.noreply.github.com>
2022-12-20 19:20:15 +0200

    Add missions page ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import './App.css';
+import Missions from './pages/missions';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Missions />
+    </div>
+  );
 }
 
 export default App;
